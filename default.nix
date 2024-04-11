4614b7fd934f466d2682011dcb89db4022a31142
Lucas Farias <lucasfnf@gmail.com>
2024-04-11 17:49:59 -0300

    Attempt 2

--- default.nix
+++ default.nix
@@ -1,22 +1,34 @@
-{ pkgs ? import <nixpkgs> {} }: {
-  appimage = pkgs.appimageTools.wrapType2 {
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
+  pname = "awakened-poe-trade";
+  version = "3.24.10002";
+
+  src = pkgs.appimageTools.wrapType2 {
     name = "awakened-poe-trade";
     version = "3.24.10002";
     src = pkgs.fetchurl {
       url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.24.10002/Awakened-PoE-Trade-3.24.10002.AppImage";
       hash = "sha256-ieRBYrtpB8GgnDDy+fDuwamix5syRH3NG5jE5UoGg5A=";
     };
-
-    meta = with pkgs.lib; {
-      homepage = "https://github.com/SnosMe/awakened-poe-trade";
-      description = "💲 🔨 Path of Exile trading app for price checking";
-      platforms = platforms.linux;
-    };
   };
-  dektopItem = pkgs.makeDesktopItem {
-    name = "awakened-poe-trade";
-    exec = "awakened-poe-trade";
-    desktopName = "AwakenedPoETrade";
-    genericName = "Awakened PoE Trade";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/bin/* $out/bin
+  '';
+
+  desktopItems = [
+    (pkgs.makeDesktopItem {
+      name = "awakened-poe-trade";
+      exec = "awakened-poe-trade";
+      desktopName = "AwakenedPoETrade";
+      genericName = "Awakened PoE Trade";
+    })
+  ];
+
+  meta = with pkgs.lib; {
+    homepage = "https://github.com/SnosMe/awakened-poe-trade";
+    description = "💲 🔨 Path of Exile trading app for price checking";
+    platforms = platforms.linux;
   };
 }
