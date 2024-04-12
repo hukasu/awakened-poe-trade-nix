015457d42760f4d80b97ee030ed15d039046dc93
Lucas Farias <lucasfnf@gmail.com>
2024-04-12 13:21:34 -0300

    Attempt 3

---
+++ awakened-poe-trade.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  appimage = pkgs.appimageTools.wrapType2 {
+    pname = "awakened-poe-trade";
+    version = "3.24.10002";
+    src = pkgs.fetchurl {
+      url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.24.10002/Awakened-PoE-Trade-3.24.10002.AppImage";
+      hash = "sha256-ieRBYrtpB8GgnDDy+fDuwamix5syRH3NG5jE5UoGg5A=";
+    };
+    meta = {
+      homepage = "https://github.com/SnosMe/awakened-poe-trade";
+      description = "💲 🔨 Path of Exile trading app for price checking";
+      platforms =  pkgs.lib.platforms.linux;
+    };
+  };
+  desktopItem = pkgs.makeDesktopItem {
+    name = appimage.name;
+    exec = appimage.name;
+    desktopName = "Awakened PoE Trade";
+    comment = appimage.meta.description;
+  };
+in
+  { inherit appimage desktopItem; }
--- default.nix
+++ default.nix
@@ -1,34 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.stdenv.mkDerivation {
-  pname = "awakened-poe-trade";
-  version = "3.24.10002";
-
-  src = pkgs.appimageTools.wrapType2 {
-    name = "awakened-poe-trade";
-    version = "3.24.10002";
-    src = pkgs.fetchurl {
-      url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.24.10002/Awakened-PoE-Trade-3.24.10002.AppImage";
-      hash = "sha256-ieRBYrtpB8GgnDDy+fDuwamix5syRH3NG5jE5UoGg5A=";
-    };
-  };
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src/bin/* $out/bin
-  '';
-
-  desktopItems = [
-    (pkgs.makeDesktopItem {
-      name = "awakened-poe-trade";
-      exec = "awakened-poe-trade";
-      desktopName = "AwakenedPoETrade";
-      genericName = "Awakened PoE Trade";
-    })
-  ];
-
-  meta = with pkgs.lib; {
-    homepage = "https://github.com/SnosMe/awakened-poe-trade";
-    description = "💲 🔨 Path of Exile trading app for price checking";
-    platforms = platforms.linux;
-  };
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+  items = pkgs.callPackage ./awakened-poe-trade.nix { };
+in {
+  awakened-poe-trade = items.appimage;
+  desktopItem = items.desktopItem;
 }
