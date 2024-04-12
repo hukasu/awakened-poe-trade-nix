f2d75ce0c91d5658778856b30d6a87d487a86bb9
Lucas Farias <lucasfnf@gmail.com>
2024-04-12 16:58:56 -0300

    Testing with only one derivation

--- default.nix
+++ default.nix
@@ -2,4 +2,4 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
   awakened-poe-trade = pkgs.callPackage ./awakened-poe-trade.nix { };
-in [ awakened-poe-trade.appimage awakened-poe-trade.desktopItem ]
+in awakened-poe-trade.appimage
