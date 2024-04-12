a405148703fc39ed86afe70437016551caa938d8
Lucas Farias <lucasfnf@gmail.com>
2024-04-12 17:02:26 -0300

    Attempt 6

--- default.nix
+++ default.nix
@@ -2,4 +2,7 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
   awakened-poe-trade = pkgs.callPackage ./awakened-poe-trade.nix { };
-in awakened-poe-trade.appimage
+in {
+  app = awakened-poe-trade.appimage;
+  desktop = awakened-poe-trade.desktopItem;
+}
