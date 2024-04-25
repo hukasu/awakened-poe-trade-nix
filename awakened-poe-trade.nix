5edc9d76aa9a49ca8e12a5b836ca0ce02558b1a1
Lucas Farias <lucasfnf@gmail.com>
2024-04-25 18:32:51 -0300

    Update to 3.24.10004

--- awakened-poe-trade.nix
+++ awakened-poe-trade.nix
@@ -2,10 +2,10 @@
 let
   appimage = pkgs.appimageTools.wrapType2 {
     pname = "awakened-poe-trade";
-    version = "3.24.10003";
+    version = "3.24.10004";
     src = pkgs.fetchurl {
-      url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.24.10003/Awakened-PoE-Trade-3.24.10003.AppImage";
-      hash = "sha256-f23FALvkvHNeJnaeU3kp1aAS+qXQG5mMlQ9VyWxi2rA=";
+      url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.24.10004/Awakened-PoE-Trade-3.24.10004.AppImage";
+      hash = "sha256-0FSePkDZYCU32EUpyymuSIGcRXUomIOrd38QPMaFV4E=";
     };
     meta = {
       homepage = "https://github.com/SnosMe/awakened-poe-trade";
@@ -16,6 +16,7 @@ let
   desktopItem = pkgs.makeDesktopItem {
     name = "AwakenedPoETrade";
     exec = appimage.name;
+    icon = "${appimage}/awakened-poe-trade.png";
     desktopName = "Awakened PoE Trade";
     comment = appimage.meta.description;
   };
