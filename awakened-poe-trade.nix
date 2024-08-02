4606a501bd2d44f0745115e5ca9833929d2d89c1
Lucas Farias <lucasfnf@gmail.com>
2024-08-02 16:47:14 -0300

    Update to 3.25.102

--- awakened-poe-trade.nix
+++ awakened-poe-trade.nix
@@ -2,10 +2,10 @@
 let
   appimage = pkgs.appimageTools.wrapType2 {
     pname = "awakened-poe-trade";
-    version = "3.25.101";
+    version = "3.25.102";
     src = pkgs.fetchurl {
-      url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.25.101/Awakened-PoE-Trade-3.25.101.AppImage";
-      hash = "sha256-n/lv+sA9f0IeQ9ntgzemEAzO9GMN08u/GXqPAzmiqK4=";
+      url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.25.102/Awakened-PoE-Trade-3.25.102.AppImage";
+      hash = "sha256-lcdKJ+B8NQmyMsv+76+eeESSrfR/7Mq6svO5VKaoNUY=";
     };
     meta = {
       homepage = "https://github.com/SnosMe/awakened-poe-trade";
